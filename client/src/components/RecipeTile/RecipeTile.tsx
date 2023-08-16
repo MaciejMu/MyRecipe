9fa8f14780c95291d6cc77dd2643259b984e50b6
Maciej Muter <116006124+MaciejMu@users.noreply.github.com>
2023-08-16 23:38:16 +0200

    add link to recipeTile

--- client/src/components/RecipeTile/RecipeTile.tsx
+++ client/src/components/RecipeTile/RecipeTile.tsx
@@ -3,26 +3,29 @@ import SaveRecipeButton from "../SaveRecipeButton/SaveRecipeButton";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import style from "./RecipeTile.module.scss";
 import { RecipeProps } from "../../pages/Home";
+import { Link } from "react-router-dom";
 
 const RecipeTile = ({ recipe }: { recipe: RecipeProps }) => {
   return (
-    <div className={style.tile}>
-      <div className={style.nameContainer}>
-        <h2>{recipe.name.toUpperCase()}</h2>
+    <Link to={`/${recipe._id}`}>
+      <div className={style.tile}>
+        <div className={style.nameContainer}>
+          <h2>{recipe.name.toUpperCase()}</h2>
+        </div>
+        <div className={style.topContianer}>
+          <SaveRecipeButton recipeId={recipe._id} />
+          <b>
+            <FontAwesomeIcon icon={faClock} className={style.clockIcon} />
+            {recipe.cookingTime}min
+          </b>
+        </div>
+        <img
+          src={recipe.imageUrl}
+          alt={recipe.name}
+          className={style.recipeImage}
+        />
       </div>
-      <div className={style.topContianer}>
-        <SaveRecipeButton recipeId={recipe._id} />
-        <b>
-          <FontAwesomeIcon icon={faClock} className={style.clockIcon} />
-          {recipe.cookingTime}min
-        </b>
-      </div>
-      <img
-        src={recipe.imageUrl}
-        alt={recipe.name}
-        className={style.recipeImage}
-      />
-    </div>
+    </Link>
   );
 };
 
