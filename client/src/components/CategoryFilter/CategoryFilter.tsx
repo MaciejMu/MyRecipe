665e7bd2d4ba77df378db3f0f075f68c43a7b49e
Maciej Muter <116006124+MaciejMu@users.noreply.github.com>
2023-08-23 03:27:43 +0200

    change categoryFilter on hover

--- client/src/components/CategoryFilter/CategoryFilter.tsx
+++ client/src/components/CategoryFilter/CategoryFilter.tsx
@@ -1,26 +1,43 @@
-import { Link } from "react-router-dom";
+import { Link, useSearchParams } from "react-router-dom";
 import style from "./CategoryFilter.module.scss";
+import clsx from "clsx";
+
+const cartegories = [
+  "Breakfast-and-Brunch",
+  "Lunch",
+  "Healty",
+  "Appetizers-and-Snacks",
+  "Salad",
+  "Side-Dishes",
+  "Soup",
+  "Bread",
+  "Drinks",
+  "Desserts",
+];
 
 const CategoryFilter = () => {
+  const [searchParams] = useSearchParams();
+  const currentCategory = searchParams.get("category");
   const createLink = (category: string) => {
-    return `/?page=1&limit=8&category=${category}`;
+    return (
+      <Link
+        to={`/?page=1&limit=8&category=${category}`}
+        className={clsx(category === currentCategory && style.current)}
+      >
+        {category.split("-").join(" ")}
+      </Link>
+    );
   };
 
   return (
     <section className={style.container}>
-      <Link to={"/?page=1&limit=8"}>All</Link>
-      <Link to={createLink("Breakfast-and-Brunch")}>Breakfast and Brunch</Link>
-      <Link to={createLink("Lunch")}>Lunch</Link>
-      <Link to={createLink("Healty")}>Healty</Link>
-      <Link to={createLink("Appetizers-and-Snacks")}>
-        Appetizers and Snacks
+      <Link
+        to={`/?page=1&limit=8`}
+        className={clsx(!currentCategory && style.current)}
+      >
+        All
       </Link>
-      <Link to={createLink("Salad")}>Salad</Link>
-      <Link to={createLink("Side-Dishes")}>Side Dishes</Link>
-      <Link to={createLink("Soup")}>Soup</Link>
-      <Link to={createLink("Bread")}>Bread</Link>
-      <Link to={createLink("Drinks")}>Drinks</Link>
-      <Link to={createLink("Desserts")}>Desserts</Link>
+      {cartegories.map((category) => createLink(category))}
     </section>
   );
 };
