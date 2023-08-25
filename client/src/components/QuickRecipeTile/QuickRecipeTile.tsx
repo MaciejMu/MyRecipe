67f67831c22bd59171a3d238560810b826b0eecc
Maciej Muter <116006124+MaciejMu@users.noreply.github.com>
2023-08-25 03:23:38 +0200

    fix mobile

--- client/src/components/QuickRecipeTile/QuickRecipeTile.tsx
+++ client/src/components/QuickRecipeTile/QuickRecipeTile.tsx
@@ -12,32 +12,29 @@ import clsx from "clsx";
 
 const QuickRecipeTile = ({ recipe }: { recipe: RecipeProps }) => {
   const [isActive, setIsActive] = useState(false);
+
+  const handleClick = () => {
+    setIsActive((prev) => !prev);
+  };
+
   return (
     <section className={style.section}>
       <hr />
       <div>
         <p>{recipe.cookingTime}"</p>
         <img src={recipe.imageUrl} />
-        <span>
+        <span onClick={handleClick}>
           <h4>{recipe.name}</h4>
+          {isActive ? (
+            <FontAwesomeIcon icon={faChevronUp} className={style.arrow} />
+          ) : (
+            <FontAwesomeIcon icon={faChevronDown} className={style.arrow} />
+          )}
         </span>
-        {isActive ? (
-          <FontAwesomeIcon
-            icon={faChevronUp}
-            className={style.arrow}
-            onClick={() => setIsActive(false)}
-          />
-        ) : (
-          <FontAwesomeIcon
-            icon={faChevronDown}
-            className={style.arrow}
-            onClick={() => setIsActive(true)}
-          />
-        )}
       </div>
-      <ul className={clsx(style.ingredientsList, isActive && style.active)}>
-        {recipe.ingredients.map((ingredient) => (
-          <li>{ingredient}</li>
+      <ul className={clsx(isActive && style.active)}>
+        {recipe.ingredients.map((ingredient, i) => (
+          <li key={i}>{ingredient}</li>
         ))}
       </ul>
       <Link to={`/${recipe._id}`}>
