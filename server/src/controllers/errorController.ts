2d83082613688feb9e7ad25bfdb28ec0b7d80c1f
Maciej Muter <116006124+MaciejMu@users.noreply.github.com>
2023-09-03 23:59:42 +0200

    handling duplicates

--- server/src/controllers/errorController.ts
+++ server/src/controllers/errorController.ts
@@ -1,4 +1,23 @@
 import { NextFunction, Request, Response } from "express";
+import createAppError from "../utilis/appError";
+
+const handleCastError = (err: unknown | any) => {
+  const message = `Invalid ${err.path}: ${err.value}`;
+  return createAppError(message, 400);
+};
+
+const handleDuplicate = (err: unknown | any) => {
+  const value = err.errmsg.match(/(["'])(?:(?=(\\?))\2.)*?\1/)[0];
+  const message = `Duplicate fields value: ${value}, Please use another value!`;
+  return createAppError(message, 400);
+};
+
+const handleValidationError = (err: unknown | any) => {
+  const errors = Object.values(err.errors).map((el) => el.message);
+  console.log(errors);
+  const message = `Invalid data! ${errors.join(". ")}`;
+  return createAppError(message, 400);
+};
 
 const errorHandlerController = (
   err: any,
@@ -9,10 +28,29 @@ const errorHandlerController = (
   err.statusCode = err.statusCode || 500;
   err.status = err.status || "error";
 
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
+  // res.status(err.statusCode).json({
+  //   status: err.status,
+  //   message: err.message,
+  // });
+
+  let error = { ...err };
+
+  if (error.name === "CastError") error = handleCastError(error);
+  if (error.code === 11000) error = handleDuplicate(error);
+  if (error.name === "ValidationError") error = handleValidationError(error);
+
+  if (error.isOperitional) {
+    res.status(error.statusCode).json({
+      status: error.status,
+      message: error.message,
+    });
+  } else {
+    console.log("ERROR:", error);
+    res.status(500).json({
+      status: "error",
+      message: "Something went wrong!",
+    });
+  }
 };
 
 export default errorHandlerController;
