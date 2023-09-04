1b89a174eea6ac1323db296f52dff3b81eee1ffb
Maciej Muter <116006124+MaciejMu@users.noreply.github.com>
2023-09-05 00:35:40 +0200

    fix inputs and some ui changes

--- client/src/components/LoginForm/LoginForm.tsx
+++ client/src/components/LoginForm/LoginForm.tsx
@@ -1,10 +1,11 @@
-import axios from "axios";
+import axios, { AxiosError } from "axios";
 import { useState } from "react";
 import { useCookies } from "react-cookie";
 import { Link, useNavigate } from "react-router-dom";
 import { BeatLoader } from "react-spinners";
 import Button from "../Button/Button";
 import style from "./LoginForm.module.scss";
+import Modal from "../Modal/Modal";
 
 const Login = () => {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
@@ -12,7 +13,17 @@ const Login = () => {
 
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [modalText, setModalText] = useState("");
+  const [showModal, setShowModal] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
+
+  const openModal = () => {
+    setShowModal(true);
+  };
+
+  const closeModal = () => {
+    setShowModal(false);
+  };
 
   const navigate = useNavigate();
 
@@ -26,50 +37,65 @@ const Login = () => {
       });
       setCookies("access_token", result.data.token);
       window.localStorage.setItem("userID", result.data.userID);
-      navigate("/");
+      setModalText("You are successfully logged in");
+      openModal();
+      setTimeout(() => {
+        navigate("/");
+      }, 1500);
     } catch (error) {
-      console.log(error);
+      if (error instanceof AxiosError) {
+        setModalText(error.response?.data.message);
+        openModal();
+      }
+      setIsLoading(false);
     }
-    setIsLoading(false);
   };
 
   return (
-    <section className={style.section}>
-      <form onSubmit={handleSubmit}>
-        <h2>Login</h2>
-        <div className="form-group">
-          <label htmlFor="username">Username:</label>
-          <input
-            required
-            type="text"
-            id="username"
-            value={username}
-            onChange={(event) => setUsername(event.target.value)}
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="password">Password:</label>
-          <input
-            required
-            type="password"
-            id="password"
-            value={password}
-            onChange={(event) => setPassword(event.target.value)}
-          />
-        </div>
-        {isLoading ? (
-          <BeatLoader color="#000000" />
-        ) : (
-          <Button type="submit" disabled={isLoading}>
-            Login
-          </Button>
-        )}
-      </form>
-      <span>
-        <p>Don't have an account?</p>
-        <Link to="/register">Join now</Link>
-      </span>
-    </section>
+    <>
+      <Modal
+        showModal={showModal}
+        closeModal={closeModal}
+        modalText={modalText}
+      />
+      <section className={style.section}>
+        <form onSubmit={handleSubmit}>
+          <h2>Login</h2>
+          <div className="form-group">
+            <label htmlFor="username">Username:</label>
+            <input
+              required
+              type="text"
+              id="username"
+              value={username.trim()}
+              onChange={(event) => setUsername(event.target.value)}
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="password">Password:</label>
+            <input
+              required
+              type="password"
+              id="password"
+              value={password.trim()}
+              onChange={(event) => setPassword(event.target.value)}
+            />
+          </div>
+          {isLoading ? (
+            <BeatLoader color="#000000" />
+          ) : (
+            <Button type="submit" disabled={isLoading}>
+              Login
+            </Button>
+          )}
+        </form>
+        <span>
+          <p>Don't have an account?</p>
+          <Link to="/register">Join now</Link>
+        </span>
+      </section>
+    </>
   );
 };
+
 export default Login;
--- client/src/components/RegisterFrom/RegisterForm.tsx
+++ client/src/components/RegisterFrom/RegisterForm.tsx
@@ -1,14 +1,26 @@
-import axios from "axios";
+import axios, { AxiosError } from "axios";
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { BeatLoader } from "react-spinners";
 import style from "./RegisterForm.module.scss";
 import Button from "../Button/Button";
+import Modal from "../Modal/Modal";
 
 const RegisterForm = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [modalText, setModalText] = useState("");
+  const [showModal, setShowModal] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
+
+  const openModal = () => {
+    setShowModal(true);
+  };
+
+  const closeModal = () => {
+    setShowModal(false);
+  };
+
   const navigate = useNavigate();
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
@@ -19,52 +31,68 @@ const RegisterForm = () => {
         username,
         password,
       });
-      alert("Registration Completed! Now login.");
-      navigate("/login");
+      setModalText(
+        "Registration Completed! You will be redirected to the login page."
+      );
+      openModal();
+      setTimeout(() => {
+        navigate("/login");
+      }, 3000);
     } catch (error) {
-      console.error(error);
+      if (error instanceof AxiosError) {
+        setModalText(error.response?.data.message);
+        openModal();
+      }
     }
+
     setIsLoading(false);
   };
 
   return (
-    <section className={style.section}>
-      <form onSubmit={handleSubmit}>
-        <h2>Register</h2>
-        <div>
-          <label htmlFor="username">Username:</label>
-          <input
-            required
-            type="text"
-            id="username"
-            maxLength={10}
-            value={username}
-            onChange={(event) => setUsername(event.target.value)}
-          />
-        </div>
-        <div>
-          <label htmlFor="password">Password:</label>
-          <input
-            required
-            type="password"
-            id="password"
-            value={password}
-            onChange={(event) => setPassword(event.target.value)}
-          />
-        </div>
-        {isLoading ? (
-          <BeatLoader color="#000000" />
-        ) : (
-          <Button type="submit" disabled={isLoading}>
-            Register
-          </Button>
-        )}
-      </form>
-      <span>
-        <p>Have an account?</p>
-        <Link to="/login">Log in</Link>
-      </span>
-    </section>
+    <>
+      <Modal
+        showModal={showModal}
+        closeModal={closeModal}
+        modalText={modalText}
+      />
+      <section className={style.section}>
+        <form onSubmit={handleSubmit}>
+          <h2>Register</h2>
+          <div>
+            <label htmlFor="username">Username:</label>
+            <input
+              required
+              type="text"
+              id="username"
+              maxLength={10}
+              value={username.trim()}
+              onChange={(event) => setUsername(event.target.value)}
+            />
+          </div>
+          <div>
+            <label htmlFor="password">Password:</label>
+            <input
+              required
+              type="password"
+              id="password"
+              value={password.trim()}
+              onChange={(event) => setPassword(event.target.value)}
+            />
+          </div>
+          {isLoading ? (
+            <BeatLoader color="#000000" />
+          ) : (
+            <Button type="submit" disabled={isLoading}>
+              Register
+            </Button>
+          )}
+        </form>
+        <span>
+          <p>Have an account?</p>
+          <Link to="/login">Log in</Link>
+        </span>
+      </section>
+    </>
   );
 };
 
