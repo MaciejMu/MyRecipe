81a56d334905a2c7ef7af302e029688de078a4ae
Maciej Muter <116006124+MaciejMu@users.noreply.github.com>
2023-09-05 18:19:05 +0200

    build fixes

--- client/src/pages/home.tsx
+++ client/src/pages/home.tsx
@@ -4,19 +4,6 @@ import RecipesTilesGrid from "../components/RecipesTilesGrid/RecipesTilesGrid";
 import HeroBanner from "../components/HeroBanner/HeroBanner";
 import NewsletterBanner from "../components/NewsletterBanner/NewsletterBanner";
 
-export type RecipeProps = {
-  _id: string;
-  name: string;
-  description: string;
-  ingredients: string[];
-  instructions: string[];
-  imageUrl: string;
-  cookingTime: number;
-  creator: string;
-  likesCounter: number;
-  category: string;
-};
-
 const Home = () => {
   useEffect(() => {
     document.title = "MyRecipe";
